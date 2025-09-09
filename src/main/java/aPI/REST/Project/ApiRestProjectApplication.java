50ae7f1923c21163c34622601905ef970630db97
Derek Meneghel <derekmeneghel2004@gmail.com>
2025-09-08 21:10:08 -0300

    First steps

---
+++ src/main/java/aPI/REST/Project/ApiRestProjectApplication.java
@@ -0,0 +1,13 @@
+package aPI.REST.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Estudos/API/REST/Project/ApiRestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Estudos.API.REST.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
